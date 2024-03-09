6093333b085a8abee78bdbb6a7de2374ad7d700b
Eva Lateltin <evalateltin@gmail.com>
2024-03-09 16:42:54 +0100

    fix: improve code readability

--- blog/article/utils.py
+++ blog/article/utils.py
@@ -7,7 +7,10 @@ def slugify_instance_title(instance):
     conditions = Q(slug__regex=r'{}-[0-9]+'.format(slug)) | Q(slug=slug)
     Klass = instance.__class__
     articles = Klass.objects.filter(conditions)
-    nums = [int(article.slug.split('-')[-1])
-            for article in articles if article.slug.split('-')[-1].isdigit()]
+    nums = []
+    for article in articles:
+        slug_ending = article.slug.split('-')[-1]
+        if slug_ending.isdigit():
+            nums.append(int(slug_ending))
     increment_num = 0 if not nums else max(nums)
     return f'{slug}-{increment_num + 1}'
