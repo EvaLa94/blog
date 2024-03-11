ccfe7e850941a62db3c4429baad67a1c30b0fbce
Eva Lateltin <evalateltin@gmail.com>
2024-03-11 22:29:22 +0100

    feat: implement html file structure

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -55,7 +55,10 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['category'],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'category',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'category' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
